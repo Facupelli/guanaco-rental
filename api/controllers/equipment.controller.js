5a586d98f27d17c7c16877ae49fb8090fd50b78a
Facupelli <facundopellicer4@gmail.com>
2022-08-02 13:39:37 -0300

    equipmnet by category name

--- api/controllers/equipment.controller.js
+++ api/controllers/equipment.controller.js
@@ -2,8 +2,24 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function getEquipment(req, res, next) {
-    const equipment = await prisma.equipment.findMany({});
-    res.json(equipment);
+  const { category } = req.query;
+
+  if (category) {
+    console.log(category);
+    const equipment = await prisma.category.findMany({
+      where: {
+        name: category,
+      },
+      include: {
+        equipments: true,
+      },
+    });
+    res.json(equipment[0].equipments);
+    return;
+  }
+
+  const equipment = await prisma.equipment.findMany({});
+  res.json(equipment);
 }
 
 module.exports = { getEquipment };
