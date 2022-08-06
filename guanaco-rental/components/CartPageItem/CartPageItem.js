4693be17413848c86fe1ff5bae8d5d8e1791322f
Facundo Pellicer <facundopellicer4@gmail.com>
2022-08-06 20:19:12 -0300

    cartPage components & styles

--- guanaco-rental/components/CartPageItem/CartPageItem.js
+++ guanaco-rental/components/CartPageItem/CartPageItem.js
@@ -1,9 +1,17 @@
-import ItemCounter from "../../CartModal/CartItem/ItemCounter/ItemCounter";
-import { formatPrice } from "../../../utils/price_formater";
+import ItemCounter from "../CartModal/CartItem/ItemCounter/ItemCounter";
+import { formatPrice } from "../../utils/price_formater";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../redux/features/cart/cartSlice";
 
 import s from "./CartPageItem.module.scss";
 
 export default function CartPageItem({ item }) {
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = () => {
+    dispatch(removeFromCart(item.id));
+  };
+
   return (
     <div className={s.item_container}>
       <div className={s.item_title}>
@@ -21,6 +29,9 @@ export default function CartPageItem({ item }) {
           ? formatPrice(item.quantity * item.price)
           : formatPrice(item.price)}
       </p>
+      <button type="button" onClick={handleRemoveFromCart}>
+        X
+      </button>
     </div>
   );
 }
--- guanaco-rental/pages/cart/index.js
+++ guanaco-rental/pages/cart/index.js
@@ -1,14 +1,22 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useSelector } from "react-redux";
-import CartPageItem from "../../components/Bookeable/CartPageItem/CartPageItem";
+import CartPageItem from "../../components/CartPageItem/CartPageItem";
 import Nav from "../../components/Nav/Nav";
 
 import s from "../../styles/CartPage.module.scss";
+import { formatPrice } from "../../utils/price_formater";
 
 export default function CartPage() {
   const cart = useSelector((state) => state.cart.items);
   const date = useSelector((state) => state.date.date_range);
+
+  const getTotalPrice = () => {
+    const totalPrice = cart.reduce((curr, acc) => {
+      return curr + acc.price;
+    }, 0);
+    return formatPrice(totalPrice);
+  };
 
   return (
     <div>
@@ -32,24 +40,28 @@ export default function CartPage() {
             cart.length > 0 &&
             cart.map((item) => <CartPageItem item={item} />)}
         </div>
-        <div className={s.summary_wrapper}>
-          <div className={s.summary}>
-            {date && date.length > 0 ? (
-              <div>
-                <p>Retiro:</p>
-                <p>{date && date.length > 0 && date[0]}</p>
-                <p>Devolución:</p>
-                <p>{date && date.length > 0 && date[1]}</p>
-              </div>
-            ) : (
-              <button>Seleccionar Fecha</button>
-            )}
+        <div className={s.summary}>
+          {date && date.length > 0 ? (
+            <div>
+              <p>Retiro:</p>
+              <p>{date && date.length > 0 && date[0]}</p>
+              <p>Devolución:</p>
+              <p>{date && date.length > 0 && date[1]}</p>
+            </div>
+          ) : (
+            <button>Seleccionar Fecha</button>
+          )}
+          <div className={s.btns_wrapper}>
             <button type="button" disabled={date.length <= 0 ? true : false}>
               agendar pedido
             </button>
             <Link href="/">
               <button type="button">continuar alquilando</button>
             </Link>
+          </div>
+          <div className={s.total_price_wrapper}>
+            <p>Total:</p>
+            <p>{cart && cart.length > 0 && getTotalPrice()}</p>
           </div>
         </div>
       </main>
