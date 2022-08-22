ca63c95be935601c9f48ec65499b56cce7bebba7
Facupelli <facundopellicer4@gmail.com>
2022-08-22 16:52:01 -0300

    fixes

--- guanaco-rental/components/Bookeable/EquipmentSearchBar/EquipmentSearchBar.js
+++ guanaco-rental/components/Bookeable/EquipmentSearchBar/EquipmentSearchBar.js
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { fetchEquipment } from "../../../redux/features/equipment/equipmentSlice";
 import s from "./EquipmentSearchBar.module.scss";
 
-export default function EquipmentSearchBar({filters}) {
+export default function EquipmentSearchBar({ filters }) {
   const dispatch = useDispatch();
   const {
     register,
@@ -16,15 +16,15 @@ export default function EquipmentSearchBar({filters}) {
 
   const search = watch("search");
 
+  console.log(search);
+
   useEffect(() => {
-    if (search) {
-      dispatch(fetchEquipment(filters.category, null, search));
-    }
+    dispatch(fetchEquipment(filters.category, null, search));
   }, [search]);
 
   return (
-    <form className={s.form}>
-      <input type="text" placeholder="Buscar equipos" {...register("search")}/>
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Buscar equipos" {...register("search")} />
     </form>
   );
 }
