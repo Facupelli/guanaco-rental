cea7495405c4611e87352c24344861b33f0d9154
Facupelli <facundopellicer4@gmail.com>
2022-09-13 20:59:30 -0300

    couponCard style

--- guanaco-rental/components/AdminCouponCard/AdminCouponCard.js
+++ guanaco-rental/components/AdminCouponCard/AdminCouponCard.js
@@ -4,10 +4,10 @@ export default function AdminCouponCard({ coupon, danger }) {
   return (
     <div className={`${s.card_container} ${danger ? s.danger_border : ""}`}>
       <p className={s.bold}>{coupon.name}</p>
-      <p>{coupon.discount}%</p>
-      {coupon.maxOrders && <p>max: {coupon.maxOrders}</p>}
+      <p>Descuento: <span className={s.bold_600}>{coupon.discount}%</span></p>
+      {coupon.maxOrders && <p>Max: {coupon.maxOrders}</p>}
       {coupon.expirationDate && (
-        <p>fecha: {new Date(coupon.expirationDate).toLocaleDateString()}</p>
+        <p>Fecha: {new Date(coupon.expirationDate).toLocaleDateString()}</p>
       )}
     </div>
   );
