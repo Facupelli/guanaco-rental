4c08e7245929e30fe7976734393d49ab204632b2
Facupelli <facundopellicer4@gmail.com>
2022-09-17 13:42:09 -0300

    check google

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 const allowedOrigins = [
   "http://localhost:3000",
   "https://guanaco-rental.vercel.app",
+  "https://guanaco-rental.vercel.app/book",
+  "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?client_id=461944679741-ua6d85l6i3521oo9fb007jm1ahofpik5.apps.googleusercontent.com&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fguanaco-rental.vercel.app%2Fapi%2Fauth%2Fcallback%2Fgoogle&state=8kaj641eEK6U6xPUBLQg3yTV6Tu1Nl5Qe4zCtl5P21Q&code_challenge=qJuAG_QkL7H6aQu9-WTQR49HbxB-qILWfalXKipRMF4&code_challenge_method=S256&flowName=GeneralOAuthFlow",
 ];
 
 app.use(
