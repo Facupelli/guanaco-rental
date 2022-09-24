7010fcf23156c6f3660e7cb1b5a663b822ed28a8
Facupelli <facundopellicer4@gmail.com>
2022-09-24 18:36:39 -0300

    fixed, remove log

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -145,9 +145,6 @@ async function postOrder(req, res, next) {
     const mailSentToGuanaco = await sendMail(mailData);
     const mailSentToClient = await sendMail(clientData, true);
 
-    console.log(mailSentToGuanaco)
-    console.log(mailSentToClient)
-
     // const msgData = {
     //   phone: orderData.user.phone,
     //   fullName: orderData.user.fullName,
