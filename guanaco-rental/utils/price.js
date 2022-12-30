83c453f19fe1d8167f671a398fef974875003206
Facundo Pellicer <facundopellicer4@gmail.com>
2022-12-30 13:52:40 -0300

    remove unused code

--- guanaco-rental/utils/price.js
+++ guanaco-rental/utils/price.js
@@ -42,51 +42,6 @@ export const getEachEarnings = (orders) => {
   };
 };
 
-export const getOwnerEarningsByOrder = (order) => {
-  let totalFederico = 0;
-  let totalOscar = 0;
-  let totalSub = 0;
-
-  const workingsDays = getWorkingTotalDays(
-    order.booking.dates,
-    order.booking.pickupHour
-  );
-
-  for (let gear of order.equipments) {
-    const gearQty = gear.bookings.filter(
-      (book) => book.bookId === order.booking.id
-    )[0].quantity;
-
-    if (gear.owner === "FEDERICO") {
-      totalFederico += workingsDays * gear.price * gearQty;
-    } else if (gear.owner === "OSCAR") {
-      totalOscar += workingsDays * gear.price * gearQty;
-    } else if (gear.owner === "SUB") {
-      totalSub += workingsDays * gear.price * gearQty * 0.7;
-      totalFederico += workingsDays * gear.price * gearQty * 0.15;
-      totalOscar += workingsDays * gear.price * gearQty * 0.15;
-    } else {
-      totalFederico += (workingsDays * gear.price * gearQty) / 2;
-      totalOscar += (workingsDays * gear.price * gearQty) / 2;
-    }
-  }
-
-  if (order.coupon) {
-    const discount = order.coupon.discount / 100;
-    return {
-      totalFederico: totalFederico * discount,
-      totalOscar: totalOscar * discount,
-      totalSub: totalSub * discount,
-    };
-  }
-
-  return {
-    totalFederico,
-    totalOscar,
-    totalSub,
-  };
-};
-
 // export const getEachTotalEarnings = (orders) => {
 //   const eachEarnings = orders.map((order) => getOwnerEarningsByOrder(order));
 
