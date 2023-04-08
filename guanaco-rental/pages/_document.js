9bf181948818afd42fd43f6024023e635817fa98
Facundo Pellicer <facundopellicer4@gmail.com>
2023-04-08 19:38:21 -0300

    GA4 tag

--- guanaco-rental/pages/_document.js
+++ guanaco-rental/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
             strategy="afterInteractive"
           />
           <Script
@@ -18,7 +18,7 @@ class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
               `,
             }}
           />
